36b54c7022d2327396a3a52bde1669698a3fce22
Ben Kirkels <ben.kirkels@gmail.com>
2025-06-11 21:28:04 +0200

    Added simple test example

---
+++ tests/test_examples.py
@@ -0,0 +1,31 @@
+from texable import Table
+
+
+def test_example1():
+    table = Table(3)
+    table.add_rows(
+        [
+            [1, 2, 0],
+            [3, 4, 0],
+            [5, 6, 0],
+        ]
+    )
+    table.headers = ("Column 1", "Column 2", "Column 3")
+    table.caption = "This is a sample table caption."
+    table.label = "tab:sample_table"
+    table.alignments[0, 2] = ["l", "r"]
+
+    expected_output = (
+        "\\begin{table}\n"
+        "  \\begin{tabular}{l c r}\n"
+        "    Column 1 & Column 2 & Column 3 \\\\\n"
+        "    1 & 2 & 0 \\\\\n"
+        "    3 & 4 & 0 \\\\\n"
+        "    5 & 6 & 0 \\\\\n"
+        "  \\end{tabular}\n"
+        "  \\caption{This is a sample table caption.}\n"
+        "  \\label{tab:sample_table}\n"
+        "\\end{table}\n"
+    )
+
+    assert str(table) == expected_output
