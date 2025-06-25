1a80846e98329d8dd6542c0339d27badfe56fd19
Ben Kirkels <ben.kirkels@gmail.com>
2025-06-25 20:08:42 +0200

    Renamed edges to borders

--- src/texable/vertical_borders.py
+++ src/texable/vertical_borders.py
@@ -1,34 +1,36 @@
-from typing import Union
-
-
 class VerticalBorders:
     def __init__(self, num_columns: int) -> None:
         self._num_columns = num_columns
-        self._edges = [False] * (num_columns + 1)
+        self._borders = [False] * (num_columns + 1)
 
     @property
-    def edges(self) -> list[bool]:
-        """Get the list of vertical edges."""
-        return self._edges.copy()
+    def borders(self) -> list[bool]:
+        """Get the list of vertical borders."""
+        return self._borders.copy()
 
     def all(self) -> None:
-        """Enable all vertical edges."""
-        for i in range(len(self._edges)):
-            self._edges[i] = True
+        """Enable all vertical borders."""
+        for i in range(len(self._borders)):
+            self._borders[i] = True
 
     def outer(self) -> None:
-        """Enable only the outer vertical edges (first and last)."""
-        self._edges[0] = True
-        self._edges[-1] = True
+        """Enable only the outer vertical borders (first and last)."""
+        self._borders[0] = True
+        self._borders[-1] = True
+
+    def inner(self) -> None:
+        """Enable only the inner vertical borders (excluding the first and last)."""
+        for i in range(1, len(self._borders) - 1):
+            self._borders[i] = True
 
     def at(self, *indexes: int) -> None:
-        """Enable a specific vertical edge or edges."""
+        """Enable a specific vertical edge or borders."""
         for index in indexes:
             if not isinstance(index, int):
                 raise TypeError("Index must be an integer.")
             if index < 0 or index >= self._num_columns + 1:
                 raise IndexError("Index out of range.")
-            self._edges[index] = True
+            self._borders[index] = True
 
     def __getitem__(self, index: int) -> str:
         """Get the status of a specific vertical edge."""
@@ -38,4 +40,4 @@ class VerticalBorders:
         if index < 0 or index >= self._num_columns + 1:
             raise IndexError("Index out of range.")
 
-        return "|" if self._edges[index] else ""
+        return "|" if self._borders[index] else ""
