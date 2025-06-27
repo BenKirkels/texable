d90a511d02ef98fc872a324daf1a655e4abeff5a
Ben Kirkels <ben.kirkels@gmail.com>
2025-06-27 16:59:22 +0200

    Setup for docs

---
+++ docs/source/conf.py
@@ -0,0 +1,43 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Texable"
+copyright = "2025, Ben Kirkels"
+author = "Ben Kirkels"
+release = "0.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.duration",
+    "sphinx.ext.napoleon",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
+
+autosummary_generate = True
+
+# Napoleon settings (optional)
+napoleon_google_docstring = True  # Enable Google style
+napoleon_numpy_docstring = False  # Disable NumPy style, unless you're using it
+
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_param = True
+napoleon_use_rtype = True
---
+++ docs/source/index.rst
@@ -0,0 +1,36 @@
+.. Texable documentation master file, created by
+   sphinx-quickstart on Fri Jun 27 15:27:35 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Texable documentation
+=====================
+
+**Texable** is a Python library designed to make the generation of LaTeX tables simple, automated, and maintainable.
+
+When working with LaTeX documents, manually updating tables as data changes can be tedious and error-prone. Texable solves this problem by allowing you to generate LaTeX tables programmatically, making it easy to update and reuse tables without touching the raw LaTeX code.
+
+.. note::
+
+   This project is under active development.
+
+Key Features
+------------
+
+- 🧱 **Generate LaTeX tables** from Python data structures or csv files.
+- 🔄 **Automatically update tables** when new data becomes available.
+- 🎯 **Customizable**: adjust column formatting, alignment, borders, captions, labels, and more.
+- 💻 **Scriptable**: ideal for integrating into data pipelines or report generation scripts.
+
+Typical Use Cases
+-----------------
+
+- Academic papers with regularly updated experimental results
+- Automated report generation
+- Reproducible research workflows
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   usage
